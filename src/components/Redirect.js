9360977ae6c9d72364aed5fd84498b9e436ce2ab
Dandan Meng <mengdandanno1@163.com>
2022-04-14 09:05:59 +0800

    add delay for redirect action

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,9 +1,16 @@
 import React, { useEffect } from "react";
 
 const Redirect = () => {
+    console.log("start1")
     useEffect(() => {
-        window.location.href = "haha";
-    }, []);
+        console.log("start2")
+        setTimeout(() => {
+            console.log("redirecting---");
+            window.location.href = "haha";
+        }, 10000);
+        console.log("end")
+
+    }, [true]);
 
     return <h5>Redirecting...</h5>;
 };
